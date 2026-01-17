2d6ad4e68d3ea4bf5fc38806eaa78a1190f6e309
John Balasing <johnbalasing.j@gmail.com>
2026-01-17 14:57:27 +0530

    added images

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React,{useEffect} from 'react'
-import './App.css';
-import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import React from "react";
+import "./App.css";
 
+const sections = [
+  { id: 1, title: "Main Section1", img: "/SXG-main.jpg" },
+  { id: 2, title: "Main Section2", img: "/SXG-sec.jpg" },
+  { id: 3, title: "Footer", img: "/SXG-footer1.jpg" },
+];
 
-const App = () => {
-
-useEffect(() => {
-    setInterval(() => {
-
-    },3000)
-},[])
-
+export default function App() {
   return (
-   <div>
-   My website
-   </div>
+    <div>
+      {sections.map((sec) => (
+        <section
+          key={sec.id}
+          className="bg-section"
+          style={{ backgroundImage: `url(${sec.img})` }}
+        >
+          <h1>{sec.title}</h1>
+        </section>
+      ))}
+    </div>
   );
 }
-
-export default App;
